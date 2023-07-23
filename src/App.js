6c40946f8ac72c2b4a1417ff091dcac944c4cfc4
pavanvanguru <pavan1122.kumar@gmail.com>
2023-07-22 20:32:29 -0500

    Task Management App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: 'Task 1', status: 'pending' },
+    { id: 2, title: 'Task 2', status: 'done' },
+    { id: 3, title: 'Task 3', status: 'pending' },
+    { id: 4, title: 'Task 4', status: 'done' },
+  ]);
+
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+
+  const handleAddTask = () => {
+    if (newTaskTitle.trim() === '') return;
+
+    const newTask = {
+      id: tasks.length + 1,
+      title: newTaskTitle,
+      status: 'pending',
+    };
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
+  };
+
+  const handleDeleteTask = (id) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  };
+
+  const handleUpdateTaskStatus = (id, status) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, status } : task
+    );
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task Management Application</h1>
+      <div>
+        <input
+          type="text"
+          value={newTaskTitle}
+          onChange={(e) => setNewTaskTitle(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.title} - {task.status}
+            <button onClick={() => handleUpdateTaskStatus(task.id, 'done')}>
+              Mark as Done
+            </button>
+            <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
